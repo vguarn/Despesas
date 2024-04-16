09338655a4fb584acb66c48223c869fcab1416b8
Usuario <Usuario@CPD02-M14>
2024-04-16 10:50:32 -0300

    Adicionar arquivos de projeto.

---
+++ Despesas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization.Advanced;
+
+namespace Despesas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {  
+            double valor = 0;
+            string resposta = "SIM";
+            while (resposta == "SIM")
+            {
+                Console.WriteLine("Informe a despesa: ");
+                string despesa = Console.ReadLine();
+                Console.WriteLine("Qual é o valor da despesa? ");
+                double precodespesa = double.Parse(Console.ReadLine());
+                valor += precodespesa;
+            }
+            Console.WriteLine("Deseja inserir outra despesa? ");
+            resposta = Console.ReadLine().ToLower();
+
+            Console.WriteLine("O total de todas as despesas é "+valor .ToString("C"));
+
+            Console.WriteLine("Quantas pessoas moram na casa? ");
+             int moradores = int.Parse(Console.ReadLine());
+
+            double media = valor / moradores;
+
+            Console.WriteLine("A média de gastos por morador é " + media.ToString("C"));
+
+            Console.ReadKey();
+
+        }
+    }
+}
